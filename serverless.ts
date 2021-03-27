fd0c63f81a0f04d00d5192cea282143d58d1aa32
Leandro <leandrofernandesvieira@gmail.com>
2021-03-27 15:19:47 -0300

    feat: add initial structure

---
+++ serverless.ts
@@ -0,0 +1,31 @@
+import type { AWS } from "@serverless/typescript";
+
+import smilesDiscover from "@functions/smiles-discover";
+
+const serverlessConfiguration: AWS = {
+  service: "smiles-discover-backend",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  plugins: ["serverless-webpack"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+    lambdaHashingVersion: "20201221",
+  },
+  // import the function via paths
+  functions: { smilesDiscover },
+};
+
+module.exports = serverlessConfiguration;
---
+++ src/functions/index.ts
@@ -0,0 +1,1 @@
+export { default as smilesDiscover } from "./smiles-discover";
